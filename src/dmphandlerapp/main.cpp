1bbcb37889522b18cace01c4eeaa02afb0d83070
xiejianjun417 <49182994+xiejianjun417@users.noreply.github.com>
2019-05-23 11:35:11 +0800

    Add files via upload

--- src/dmphandlerapp/main.cpp
+++ src/dmphandlerapp/main.cpp
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <memory.h>
+#include <string.h>
+#include <malloc.h>
 
+#include "dmphandler.h"
+
+int main(int argc, char **argv)
+{
+    RegisterDmpHandler();
+    //memcpy(NULL, "dmphandler", sizeof("dmphandler"));
+    char *buffer = (char *)malloc(5);
+    strcpy(buffer, "123");
+    strncat_s(buffer, 5, "abcdef", 5);
+    UnRegisterDmpHandler();
+    return 0;
+}
