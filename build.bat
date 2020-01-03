a1b83884f6541f13164d6243e9f93e8af969d621
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 09:37:16 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 dir "C:\Program Files (x86)"
+tree "C:\Program Files (x86)\Microsoft SDKs\"
+tree "C:\Program Files (x86)\Microsoft Visual Studio\"
+tree "C:\Program Files (x86)\MSBuild\"
+tree "C:\Program Files (x86)\Windows Kits\"
 cd src\dmphandler\
 msbuild
