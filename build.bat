f7e9d3c6f50e760064a7e5cd9b8c4ddfe8423981
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 10:29:09 +0800

    update build.bat travis.yml

--- build.bat
+++ build.bat
@@ -1,7 +1,2 @@
-dir "C:\Program Files (x86)"
-dir "C:\Program Files (x86)\Microsoft SDKs\"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\"
-dir "C:\Program Files (x86)\MSBuild\"
-dir "C:\Program Files (x86)\Windows Kits\"
 cd src\dmphandler\
-msbuild
+msbuild /toolsversion:4.0
