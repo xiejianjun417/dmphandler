915a1234d8a6f01cb5e52707d6e062d1cc2a9e33
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 17:12:49 +0800

    test rt lib

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\"
 cd src\dmphandler
 g++ -fPIC -std=c++11 -Wall -c MiniDumpHandler.cpp -o MiniDumpHandler.o
 g++ -fPIC -std=c++11 -Wall -I./include -c dmphandler.cpp -o dmphandler.o
