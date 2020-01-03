76953d213bb00adb476951047da90936b7404c37
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 16:41:14 +0800

    test gcc

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-
 cd src\dmphandler
+gcc -fPIC -std=c++11 -Wall -c MiniDumpHandler.cpp -o MiniDumpHandler.o
+gcc -fPIC -std=c++11 -Wall -c dmphandler.cpp -o dmphandler.o
+gcc -fPIC -shared dmphandler.o MiniDumpHandler.o -o dmphandler.dll
+
