6e5a87751abf17bc32f92efdc72b31ee5d565a36
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 17:06:00 +0800

    link rt lib

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd src\dmphandler
 g++ -fPIC -std=c++11 -Wall -c MiniDumpHandler.cpp -o MiniDumpHandler.o
 g++ -fPIC -std=c++11 -Wall -I./include -c dmphandler.cpp -o dmphandler.o
-g++ -fPIC -shared dmphandler.o MiniDumpHandler.o -o dmphandler.dll
+g++ -fPIC -shared -lmsvcr150 -lmsvcp150 dmphandler.o MiniDumpHandler.o -o dmphandler.dll
 
