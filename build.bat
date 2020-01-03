211670b10ec8a8f651694246ae23f8ef46ebf218
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 17:18:02 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\"
 cd src\dmphandler
 g++ -fPIC -std=c++11 -Wall -c MiniDumpHandler.cpp -o MiniDumpHandler.o
 g++ -fPIC -std=c++11 -Wall -I./include -c dmphandler.cpp -o dmphandler.o
-g++ -fPIC -shared -lmsvcr150 -lmsvcp150 dmphandler.o MiniDumpHandler.o -o dmphandler.dll
+g++ -fPIC -shared -lmsvcr140 -lmsvcp140 -L"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\" dmphandler.o MiniDumpHandler.o -o dmphandler.dll
 
