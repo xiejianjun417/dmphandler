50a11cec57cbb7d5e86006eb96645240378b5e59
xiejianjun417 <xiejianjun417@qq.com>
2020-01-03 15:39:25 +0800

    update

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\"
-dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\"
+dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\"
+dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\"
 cd src\dmphandler
