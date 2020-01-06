3a521e02dd5e339a838f17d9076b6059bd4f2cab
xiejianjun417 <xiejianjun417@qq.com>
2020-01-06 16:37:49 +0800

    更新build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
-dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
+@rem dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
 
 mingw32-make -C src\dmphandler all
-if %ERRORLEVEL% NEQ 0 goto :EOF 
+@set compRet=%ERRORLEVEL%
+@if %compRet% equ 0 (
+    exit /b %compRet%
+)
 dir src\dmphandler
 mingw32-make -C src\dmphandlerapp all
 
