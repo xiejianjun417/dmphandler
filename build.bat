62c4965395d7ce5216dcf3cb8168feae1cb8772f
xiejianjun417 <xiejianjun417@qq.com>
2020-01-06 16:27:06 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
 
-@rem g++ -fPIC -std=c++11 -Wall -c MiniDumpHandler.cpp -o MiniDumpHandler.o
-@rem g++ -fPIC -std=c++11 -Wall -I./include -c dmphandler.cpp -o dmphandler.o
-@rem g++ -fPIC -shared -lmsvcrt -L"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\" dmphandler.o MiniDumpHandler.o -o dmphandler.dll
 mingw32-make -C src\dmphandler all
-dir src\dmphandler
 if %ERRORLEVEL% NEQ 0 goto :EOF 
+dir src\dmphandler
 mingw32-make -C src\dmphandlerapp all
 
