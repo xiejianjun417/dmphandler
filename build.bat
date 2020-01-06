deb54fe49aeb630fbcd5df1d3234db99884909a4
xiejianjun417 <xiejianjun417@qq.com>
2020-01-06 14:20:09 +0800

    mingw32-make

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 @rem g++ -fPIC -std=c++11 -Wall -c MiniDumpHandler.cpp -o MiniDumpHandler.o
 @rem g++ -fPIC -std=c++11 -Wall -I./include -c dmphandler.cpp -o dmphandler.o
 @rem g++ -fPIC -shared -lmsvcrt -L"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\" dmphandler.o MiniDumpHandler.o -o dmphandler.dll
-mingw64-make -C src\dmphandler all
-mingw64-make -C src\dmphandlerapp all
+mingw32-make -C src\dmphandler all
+mingw32-make -C src\dmphandlerapp all
 
