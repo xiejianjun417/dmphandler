dbce3d85e4281e57bf4c28938a61e4467d57ff3a
xiejianjun417 <xiejianjun417@qq.com>
2020-01-06 16:48:40 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
-@rem dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
+#call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+#@rem dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
 
 mingw32-make -C src\dmphandler all
 ret=$?
