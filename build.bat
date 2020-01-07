15692ad46bab9beadf4a27574a3a3ba97bd9d842
xiejianjun417 <xiejianjun417@qq.com>
2020-01-07 15:25:14 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -2,9 +2,14 @@
 @rem dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
 dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/x86_64-w64-mingw32"
 dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/lib/gcc/x86_64-w64-mingw32/"
+cd src\dmphandler
+gcc -fPIC -Wall -g -I./include -c MiniDumpHandler.cpp -o MiniDumpHandler.o
+gcc -fPIC -Wall -g -I./include -c dmphandler.cpp -o dmphandler.o
+g++ -fPIC -shared MiniDumpHandler.o dmphandler.o -o dmphandler.dll
+dir .
 
-mingw32-make -C src\dmphandler all
-dir src\dmphandler
+@remmingw32-make -C src\dmphandler all
+@remdir src\dmphandler
 @rem mingw32-make -C src\dmphandlerapp all
 @rem dir .
 
