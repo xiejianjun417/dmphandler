f05518266660493dced31429f0bb77a17f70f0c2
xiejianjun417 <xiejianjun417@qq.com>
2020-01-07 15:31:52 +0800

    -v

--- build.bat
+++ build.bat
@@ -3,13 +3,13 @@
 dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/x86_64-w64-mingw32"
 dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/lib/gcc/x86_64-w64-mingw32/"
 cd src\dmphandler
-gcc -fPIC -Wall -g -I./include -c MiniDumpHandler.cpp -o MiniDumpHandler.o
-gcc -fPIC -Wall -g -I./include -c dmphandler.cpp -o dmphandler.o
-g++ -fPIC -shared MiniDumpHandler.o dmphandler.o -o dmphandler.dll
+g++ -fPIC -Wall -g -I./include -c MiniDumpHandler.cpp -o MiniDumpHandler.o
+g++ -fPIC -Wall -g -I./include -c dmphandler.cpp -o dmphandler.o
+g++ -v -fPIC -shared MiniDumpHandler.o dmphandler.o -o dmphandler.dll
 dir .
 
-@remmingw32-make -C src\dmphandler all
-@remdir src\dmphandler
+@rem mingw32-make -C src\dmphandler all
+@rem dir src\dmphandler
 @rem mingw32-make -C src\dmphandlerapp all
 @rem dir .
 
