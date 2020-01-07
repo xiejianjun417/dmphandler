0a50c60f6e3ebc8e6ee1d538680cca0241fabaa4
xiejianjun417 <xiejianjun417@qq.com>
2020-01-07 15:47:24 +0800

    update

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 @rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 @rem dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
-dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/x86_64-w64-mingw32"
-dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/lib/gcc/x86_64-w64-mingw32/"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/../lib/gcc/"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../../x86_64-w64-mingw32/lib/../lib/"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../../lib/"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../../x86_64-w64-mingw32/lib/"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../"
 cd src\dmphandler
 g++ -fPIC -Wall -g -I./include -c MiniDumpHandler.cpp -o MiniDumpHandler.o
 g++ -fPIC -Wall -g -I./include -c dmphandler.cpp -o dmphandler.o
