53e610f07acafc64bd2ef1e41e2a1b0f5bf78a12
xiejianjun417 <xiejianjun417@qq.com>
2020-01-07 14:08:47 +0800

    update

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 @rem dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib\"
-dir "C:/ProgramData/chocolatey/lib/mingw"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/lib/gcc"
+dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/lib/gcc/x86_64-w64-mingw32"
 dir "C:/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/"
 
 @rem mingw32-make -C src\dmphandler all
